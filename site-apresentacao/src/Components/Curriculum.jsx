6fa34699c1c9dff9bc61733e601c43e1b66b2fd5
Jessica Arruda <sicarruda@gmail.com>
2021-06-08 14:30:19 -0300

    Inicio da implementaçao do React

---
+++ site-apresentacao/src/Components/Curriculum.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Curriculum extends React.Component {
+  render() {
+    return (
+     <div>
+       <h1>Curriculum</h1>
+     </div>
+    );
+  }
+}
+export default Curriculum;
---
+++ site-apresentacao/src/Components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+        <Footer/>
+      </div>
+    );
+  }
+}
+export default Home;
